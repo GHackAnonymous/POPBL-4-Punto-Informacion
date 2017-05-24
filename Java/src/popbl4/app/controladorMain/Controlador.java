224c169af9b21e212e6b1119957190ddfe3aef2a
Eder Gomez de Segura <ederaraia@gmail.com>
2017-05-24 16:08:59 +0200

    Restructuracion

---
+++ Java/src/popbl4/app/controladorMain/Controlador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package popbl4.app.controladorMain;
+
+/**
+ *
+ * @author GHackAnonymous
+ */
+public class Controlador {
+    
+}
---
+++ Java/src/popbl4/app/controladorMain/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package popbl4.app.controladorMain;
+
+/**
+ *
+ * @author GHackAnonymous
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
