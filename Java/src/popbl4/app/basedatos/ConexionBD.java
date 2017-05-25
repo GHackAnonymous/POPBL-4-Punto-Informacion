50b95f1b21a50012dd3b9e9fc8794abfe708c3f4
Eder Gomez de Segura <ederaraia@gmail.com>
2017-05-25 09:37:31 +0200

    ConexionBD
    Creo variables  con los parametros para iniciar ua conexion 
    con la base de datos mySQL.

--- Java/src/popbl4/app/basedatos/ConexionBD.java
+++ Java/src/popbl4/app/basedatos/ConexionBD.java
@@ -5,10 +5,25 @@
  */
 package popbl4.app.basedatos;
 
+//Importar librería JDBC completa
+import java.sql.*;
+
 /**
  *
  * @author GHackAnonymous
  */
+
 public class ConexionBD {
+    
+    private String ip = "127.0.0.1";
+    private String user = "root";
+    private String pass = "";
+    private String nombreBD = "popbl4";
+    
+    private String jdbcDriver = "com.mysql.jdbc.Driver";  
+    private String urlBD = "jdbc:mysql://"+ip+"/"+nombreBD+"";
+    
+    public void Conexion(){
+    }
     
 }
