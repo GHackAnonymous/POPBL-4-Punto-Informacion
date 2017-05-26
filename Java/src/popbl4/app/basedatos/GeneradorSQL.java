5d945034175bc0f4323148fc2eb3f08d4eacee38
Eder Gomez de Segura <ederaraia@gmail.com>
2017-05-26 15:56:58 +0200

    GeneradorSQL
    Declaro las funciones que debolveran las query pero no estan 
    implementada.

--- Java/src/popbl4/app/basedatos/GeneradorSQL.java
+++ Java/src/popbl4/app/basedatos/GeneradorSQL.java
@@ -5,10 +5,51 @@
  */
 package popbl4.app.basedatos;
 
+import  popbl4.app.admin.Administrador;
+import  popbl4.app.admin.Log;
+import  popbl4.app.interactuador.Informacion;
+import  popbl4.app.sinInteraccion.Anuncio;
+
 /**
  *
  * @author GHackAnonymous
  */
 public class GeneradorSQL {
     
+    public String generaSelectAdministrador(Administrador adminstrador){
+    
+    }
+    public String generaInsertAdministrador(Administrador adminstrador){
+    
+    }
+    public String generaUpdateAdministrador(Administrador adminstrador){
+    
+    }
+    public String generaSelectAnuncios(Anuncio anuncio){
+    
+    }
+    public String generaInsertAnuncios(Anuncio anuncio){
+    
+    }
+    public String generaUpdateAnuncios(Anuncio anuncio){
+    
+    }
+    public String generaSelectInformacion(Informacion informacion){
+    
+    }
+    public String generaInsertInformacion(Informacion informacion){
+    
+    }
+    public String generaUpdateInformacion(Informacion informacion){
+    
+    }
+    public String generaSelectLog(Log log){
+    
+    }
+    public String generaInsertLog(Log log){
+    
+    }
+    public String generaUpdateLog(Log log){
+    
+    }
 }
