7f5d70022ea3c501fd7c4fd378d47c8a8049b04e
Eder Gomez de Segura <ederaraia@gmail.com>
2017-05-29 15:38:30 +0200

    ConexionRS232
    Implementacion de la funciones de lectura y escritura en al puerto
    RS232 desde la raspberry pi.

--- Java/src/popbl4/app/olimexRS232/ConexionRS232.java
+++ Java/src/popbl4/app/olimexRS232/ConexionRS232.java
@@ -5,10 +5,63 @@
  */
 package popbl4.app.olimexRS232;
 
+
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
 /**
  *
  * @author GHackAnonymous
  */
 public class ConexionRS232 {
+
+    public ConexionRS232() {
+    }
     
+    private String puertoConectado(){
+
+		String[] portNames = SerialPortList.getPortNames();
+		for(int i = 0; i < portNames.length; i++){
+			System.out.println(portNames[i]);
+		}
+		return portNames[0];
+	}
+
+	public String leerUsart(){
+
+		SerialPort serialPort = new SerialPort(puertoConectado());
+		String buffer = null;
+		
+		try {
+			serialPort.openPort();//Open serial port
+			serialPort.setParams(9600, 8, 1, 0);//Set params.
+			buffer = serialPort.readString(4);//Read 10 bytes from serial port
+			serialPort.closePort();//Close serial port
+		}
+		catch (SerialPortException ex) {
+			System.out.println(ex);
+		}
+		return buffer;
+	}
+
+	public void escribirUsart(String texto){
+		SerialPort serialPort = new SerialPort(puertoConectado());
+		try {
+			serialPort.openPort();//Open serial port
+
+			/*
+	        serialPort.setParams(SerialPort.BAUDRATE_9600, 
+	                             SerialPort.DATABITS_8,
+	                             SerialPort.STOPBITS_1,
+	                             SerialPort.PARITY_NONE);
+			 */  
+			serialPort.setParams(9600, 8, 1, 0);
+			serialPort.writeBytes(texto.getBytes());//Write data to port
+			serialPort.closePort();//Close serial port
+		}
+		catch (SerialPortException ex) {
+			System.out.println(ex);
+		}
+	}
 }
