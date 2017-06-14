568c674efe18cda75b56961c6f04f7f1b1202969
Eder Gomez de Segura <ederaraia@gmail.com>
2017-06-14 14:23:20 +0200

    Script de Base de Datos
    Modifico el script de base de datos para asi adaptar a la
    obtencion de datos de los metodos de la aplicacion.
    
    	modified:   Script BD/CrearBaseDatosYTablas.sql
    	modified:   Script BD/meterDatosEnBaseDatos.sql

--- Script BD/CrearBaseDatosYTablas.sql
+++ Script BD/CrearBaseDatosYTablas.sql
@@ -5,7 +5,7 @@ use basedatos;
 CREATE TABLE administradores
 (	id_admin int(10) NOT NULL AUTO_INCREMENT,
 	nick varchar(100) NOT NULL,
-	nombre varchar(100) NOT NULL,
+	nombre_admin varchar(100) NOT NULL,
 	clave varchar(300) NOT NULL,
 	CONSTRAINT id_admin_pk PRIMARY KEY(id_admin)
 );
@@ -44,18 +44,18 @@ CREATE TABLE anuncios
 	id_anuncios int(10) NOT NULL AUTO_INCREMENT,
 	titulo varchar(300),
 	descripcion varchar(300),
-	url_foto varchar(300),
+	url_foto_anuncio varchar(300),
 	ubicacion varchar(300),
 	contacto varchar(300),
 	horarios varchar(300),
-	fecha date,
+	fecha_anuncio varchar(300),
 	CONSTRAINT id_anuncios_pk PRIMARY KEY(id_anuncios)
 );
 
 CREATE TABLE menu
 (
 	id_menu int(10) NOT NULL AUTO_INCREMENT,
-	nombre varchar(300),
+	nombre_menu varchar(300),
 	ingedientes varchar(300),
 	precio varchar(300),
 	CONSTRAINT id_menu_pk PRIMARY KEY(id_menu)
@@ -65,36 +65,36 @@ CREATE TABLE gastronomia
 (
 	id_gastronomia int(10) NOT NULL AUTO_INCREMENT,
 	id_menu int(10) NOT NULL,
-	id_anuncios int(10) NOT NULL,
+	id_anuncios_gastronomia int(10) NOT NULL,
 	CONSTRAINT id_gastronomia_pk PRIMARY KEY(id_gastronomia),
 	CONSTRAINT id_menu_fk FOREIGN KEY(id_menu)
 		REFERENCES menu(id_menu),
-	CONSTRAINT id_anuncios_fk FOREIGN KEY(id_anuncios)
+	CONSTRAINT id_anuncios_fk FOREIGN KEY(id_anuncios_gastronomia)
 		REFERENCES anuncios(id_anuncios)
 );
 
 CREATE TABLE productos
 (
 	id_producto int(10) NOT NULL AUTO_INCREMENT,
-	nombre varchar(300),
+	nombre_producto varchar(300),
 	precio varchar(300),
-	url_foto varchar(300),
-	id_anuncios int(10) NOT NULL,
+	url_foto_producto varchar(300),
+	id_anuncios_productos int(10) NOT NULL,
 	CONSTRAINT id_producto_pk PRIMARY KEY(id_producto),
-	CONSTRAINT id_anuncios_fk2 FOREIGN KEY(id_anuncios) 
+	CONSTRAINT id_anuncios_fk2 FOREIGN KEY(id_anuncios_productos) 
 		REFERENCES anuncios(id_anuncios)
 );
 
 CREATE TABLE tiendas
 (
 	id_tiendas int(10) NOT NULL AUTO_INCREMENT,
-	nombre varchar(300),
+	nombre_tienda varchar(300),
 	id_producto int(10) NOT NULL,
-	id_anuncios int(10) NOT NULL,
+	id_anuncios_tiendas int(10) NOT NULL,
 	CONSTRAINT id_tiendas_pk PRIMARY KEY(id_tiendas),
 	CONSTRAINT id_producto_fk2 FOREIGN KEY(id_producto)
 		REFERENCES productos(id_producto),
-	CONSTRAINT id_anuncios_fk3 FOREIGN KEY(id_anuncios)
+	CONSTRAINT id_anuncios_fk3 FOREIGN KEY(id_anuncios_tiendas)
 		REFERENCES anuncios(id_anuncios)
 );
 
@@ -103,17 +103,17 @@ CREATE TABLE servicios
 	id_servicios int(10) NOT NULL AUTO_INCREMENT,
 	url_foto_servicio varchar(300),
 	precios varchar(300),
-	id_anuncios int(10) NOT NULL,
+	id_anuncios_servicios int(10) NOT NULL,
 	CONSTRAINT id_servicios_pk PRIMARY KEY(id_servicios),
-	CONSTRAINT id_anuncios_fk4 FOREIGN KEY(id_anuncios)
+	CONSTRAINT id_anuncios_fk4 FOREIGN KEY(id_anuncios_servicios)
 		REFERENCES anuncios(id_anuncios)
 );
 
 CREATE TABLE dispositivo
 (
 	id_dispositivo int(10) AUTO_INCREMENT,
-	nombre varchar(100),
-	fecha date,
+	nombre_dispositivo varchar(100),
+	fecha_dispositivo varchar(300),
 	CONSTRAINT id_dispositivo_pk PRIMARY KEY(id_dispositivo)
 );
 
--- Script BD/meterDatosEnBaseDatos.sql
+++ Script BD/meterDatosEnBaseDatos.sql
@@ -1,13 +1,13 @@
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo1', 'Algo1nombre', 'algo1Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo2', 'Algo2nombre', 'algo2Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo3', 'Algo3nombre', 'algo3Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo4', 'Algo4nombre', 'algo4Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo5', 'Algo5nombre', 'algo5Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo6', 'Algo6nombre', 'algo6Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo7', 'Algo7nombre', 'algo7Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo8', 'Algo8nombre', 'algo8Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo9', 'Algo9nombre', 'algo9Clave');
-INSERT INTO administradores (nick, nombre, clave) VALUES ('algo10', 'Algo10nombre', 'algo10Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo1', 'Algo1nombre', 'algo1Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo2', 'Algo2nombre', 'algo2Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo3', 'Algo3nombre', 'algo3Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo4', 'Algo4nombre', 'algo4Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo5', 'Algo5nombre', 'algo5Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo6', 'Algo6nombre', 'algo6Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo7', 'Algo7nombre', 'algo7Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo8', 'Algo8nombre', 'algo8Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo9', 'Algo9nombre', 'algo9Clave');
+INSERT INTO administradores (nick, nombre_admin, clave) VALUES ('algo10', 'Algo10nombre', 'algo10Clave');
 
 INSERT INTO tipoAdmin (tipo, id_admin) VALUES ('tipoalgo1', 1);
 INSERT INTO tipoAdmin (tipo, id_admin) VALUES ('tipoalgo2', 2);
@@ -36,63 +36,63 @@ INSERT INTO tipos_de_tipos_anuncios (descripcion) VALUES ('Descripcionalgo1');
 INSERT INTO tipos_de_tipos_anuncios (descripcion) VALUES ('Descripcionalgo2');
 INSERT INTO tipos_de_tipos_anuncios (descripcion) VALUES ('Descripcionalgo3');
 
-INSERT INTO anuncios (titulo, descripcion, url_foto, ubicacion, contacto, horarios, fecha) 
-	VALUES ('tituloalgo1', 'Descripcionalgo1', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', 06/23/2017);
-INSERT INTO anuncios (titulo, descripcion, url_foto, ubicacion, contacto, horarios, fecha) 
-	VALUES ('tituloalgo2', 'Descripcionalgo2', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', 06/23/2017);
-INSERT INTO anuncios (titulo, descripcion, url_foto, ubicacion, contacto, horarios, fecha) 
-	VALUES ('tituloalgo3', 'Descripcionalgo3', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', 06/23/2017);
-INSERT INTO anuncios (titulo, descripcion, url_foto, ubicacion, contacto, horarios, fecha) 
-	VALUES ('tituloalgo4', 'Descripcionalgo4', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', 06/23/2017);
-INSERT INTO anuncios (titulo, descripcion, url_foto, ubicacion, contacto, horarios, fecha) 
-	VALUES ('tituloalgo5', 'Descripcionalgo5', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', 06/23/2017);
-INSERT INTO anuncios (titulo, descripcion, url_foto, ubicacion, contacto, horarios, fecha) 
-	VALUES ('tituloalgo6', 'Descripcionalgo6', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', 06/23/2017);
-INSERT INTO anuncios (titulo, descripcion, url_foto, ubicacion, contacto, horarios, fecha) 
-	VALUES ('tituloalgo7', 'Descripcionalgo7', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', 06/23/2017);
-INSERT INTO anuncios (titulo, descripcion, url_foto, ubicacion, contacto, horarios, fecha) 
-	VALUES ('tituloalgo8', 'Descripcionalgo8', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', 06/23/2017);
+INSERT INTO anuncios (titulo, descripcion, url_foto_anuncio, ubicacion, contacto, horarios, fecha_anuncio) 
+	VALUES ('tituloalgo1', 'Descripcionalgo1', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', '06/23/2017');
+INSERT INTO anuncios (titulo, descripcion, url_foto_anuncio, ubicacion, contacto, horarios, fecha_anuncio) 
+	VALUES ('tituloalgo2', 'Descripcionalgo2', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', '06/23/2017');
+INSERT INTO anuncios (titulo, descripcion, url_foto_anuncio, ubicacion, contacto, horarios, fecha_anuncio) 
+	VALUES ('tituloalgo3', 'Descripcionalgo3', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', '06/23/2017');
+INSERT INTO anuncios (titulo, descripcion, url_foto_anuncio, ubicacion, contacto, horarios, fecha_anuncio) 
+	VALUES ('tituloalgo4', 'Descripcionalgo4', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', '06/23/2017');
+INSERT INTO anuncios (titulo, descripcion, url_foto_anuncio, ubicacion, contacto, horarios, fecha_anuncio) 
+	VALUES ('tituloalgo5', 'Descripcionalgo5', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', '06/23/2017');
+INSERT INTO anuncios (titulo, descripcion, url_foto_anuncio, ubicacion, contacto, horarios, fecha_anuncio) 
+	VALUES ('tituloalgo6', 'Descripcionalgo6', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', '06/23/2017');
+INSERT INTO anuncios (titulo, descripcion, url_foto_anuncio, ubicacion, contacto, horarios, fecha_anuncio) 
+	VALUES ('tituloalgo7', 'Descripcionalgo7', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', '06/23/2017');
+INSERT INTO anuncios (titulo, descripcion, url_foto_anuncio, ubicacion, contacto, horarios, fecha_anuncio) 
+	VALUES ('tituloalgo8', 'Descripcionalgo8', 'log.png', 'Bilbo', 'eder', '09:00 - 22:00', '06/23/2017');
 
-INSERT INTO menu (nombre, ingedientes, precio) VALUES ('nombrealgo1', 'cebolla-patata-carne', '3€');
-INSERT INTO menu (nombre, ingedientes, precio) VALUES ('nombrealgo2', 'cebolla-patata-pescado', '3€');
-INSERT INTO menu (nombre, ingedientes, precio) VALUES ('nombrealgo3', 'cebolla-patata-zanaoria', '3€');
-INSERT INTO menu (nombre, ingedientes, precio) VALUES ('nombrealgo4', 'cebolla-patata', '3€');
-INSERT INTO menu (nombre, ingedientes, precio) VALUES ('nombrealgo5', 'cebolla-patata-lechuga', '3€');
+INSERT INTO menu (nombre_menu, ingedientes, precio) VALUES ('nombrealgo1', 'cebolla-patata-carne', '3€');
+INSERT INTO menu (nombre_menu, ingedientes, precio) VALUES ('nombrealgo2', 'cebolla-patata-pescado', '3€');
+INSERT INTO menu (nombre_menu, ingedientes, precio) VALUES ('nombrealgo3', 'cebolla-patata-zanaoria', '3€');
+INSERT INTO menu (nombre_menu, ingedientes, precio) VALUES ('nombrealgo4', 'cebolla-patata', '3€');
+INSERT INTO menu (nombre_menu, ingedientes, precio) VALUES ('nombrealgo5', 'cebolla-patata-lechuga', '3€');
 
-INSERT INTO gastronomia (id_menu, id_anuncios) VALUES (1, 1);
-INSERT INTO gastronomia (id_menu, id_anuncios) VALUES (2, 2);
-INSERT INTO gastronomia (id_menu, id_anuncios) VALUES (3, 3);
-INSERT INTO gastronomia (id_menu, id_anuncios) VALUES (4, 4);
-INSERT INTO gastronomia (id_menu, id_anuncios) VALUES (5, 5);
+INSERT INTO gastronomia (id_menu, id_anuncios_gastronomia) VALUES (1, 1);
+INSERT INTO gastronomia (id_menu, id_anuncios_gastronomia) VALUES (2, 2);
+INSERT INTO gastronomia (id_menu, id_anuncios_gastronomia) VALUES (3, 3);
+INSERT INTO gastronomia (id_menu, id_anuncios_gastronomia) VALUES (4, 4);
+INSERT INTO gastronomia (id_menu, id_anuncios_gastronomia) VALUES (5, 5);
 
-INSERT INTO productos (nombre, precio, url_foto, id_anuncios) VALUES ('nombrealgo1', '4€', 'plusgreen.png', 1);
-INSERT INTO productos (nombre, precio, url_foto, id_anuncios) VALUES ('nombrealgo2', '4€', 'plusgreen.png', 2);
-INSERT INTO productos (nombre, precio, url_foto, id_anuncios) VALUES ('nombrealgo3', '4€', 'plusgreen.png', 3);
-INSERT INTO productos (nombre, precio, url_foto, id_anuncios) VALUES ('nombrealgo4', '4€', 'plusgreen.png', 4);
-INSERT INTO productos (nombre, precio, url_foto, id_anuncios) VALUES ('nombrealgo5', '4€', 'plusgreen.png', 5);
-INSERT INTO productos (nombre, precio, url_foto, id_anuncios) VALUES ('nombrealgo6', '4€', 'plusgreen.png', 6);
+INSERT INTO productos (nombre_producto, precio, url_foto_producto, id_anuncios_productos) VALUES ('nombrealgo1', '4€', 'plusgreen.png', 1);
+INSERT INTO productos (nombre_producto, precio, url_foto_producto, id_anuncios_productos) VALUES ('nombrealgo2', '4€', 'plusgreen.png', 2);
+INSERT INTO productos (nombre_producto, precio, url_foto_producto, id_anuncios_productos) VALUES ('nombrealgo3', '4€', 'plusgreen.png', 3);
+INSERT INTO productos (nombre_producto, precio, url_foto_producto, id_anuncios_productos) VALUES ('nombrealgo4', '4€', 'plusgreen.png', 4);
+INSERT INTO productos (nombre_producto, precio, url_foto_producto, id_anuncios_productos) VALUES ('nombrealgo5', '4€', 'plusgreen.png', 5);
+INSERT INTO productos (nombre_producto, precio, url_foto_producto, id_anuncios_productos) VALUES ('nombrealgo6', '4€', 'plusgreen.png', 6);
 
-INSERT INTO tiendas (nombre, id_producto, id_anuncios) VALUES ('nombrealgo1', 1, 1);
-INSERT INTO tiendas (nombre, id_producto, id_anuncios) VALUES ('nombrealgo1', 1, 2);
-INSERT INTO tiendas (nombre, id_producto, id_anuncios) VALUES ('nombrealgo1', 1, 3);
-INSERT INTO tiendas (nombre, id_producto, id_anuncios) VALUES ('nombrealgo1', 1, 4);
-INSERT INTO tiendas (nombre, id_producto, id_anuncios) VALUES ('nombrealgo1', 1, 5);
-INSERT INTO tiendas (nombre, id_producto, id_anuncios) VALUES ('nombrealgo1', 1, 6);
+INSERT INTO tiendas (nombre_tienda, id_producto, id_anuncios_tiendas) VALUES ('nombrealgo1', 1, 1);
+INSERT INTO tiendas (nombre_tienda, id_producto, id_anuncios_tiendas) VALUES ('nombrealgo1', 2, 2);
+INSERT INTO tiendas (nombre_tienda, id_producto, id_anuncios_tiendas) VALUES ('nombrealgo1', 3, 3);
+INSERT INTO tiendas (nombre_tienda, id_producto, id_anuncios_tiendas) VALUES ('nombrealgo1', 4, 4);
+INSERT INTO tiendas (nombre_tienda, id_producto, id_anuncios_tiendas) VALUES ('nombrealgo1', 5, 5);
+INSERT INTO tiendas (nombre_tienda, id_producto, id_anuncios_tiendas) VALUES ('nombrealgo1', 6, 6);
 
-INSERT INTO servicios (url_foto_servicio, precios, id_anuncios) VALUES ('gastronomia.png','5€', 1);
-INSERT INTO servicios (url_foto_servicio, precios, id_anuncios) VALUES ('gastronomia.png','5€', 2);
-INSERT INTO servicios (url_foto_servicio, precios, id_anuncios) VALUES ('gastronomia.png','5€', 3);
-INSERT INTO servicios (url_foto_servicio, precios, id_anuncios) VALUES ('gastronomia.png','5€', 4);
-INSERT INTO servicios (url_foto_servicio, precios, id_anuncios) VALUES ('gastronomia.png','5€', 5);
-INSERT INTO servicios (url_foto_servicio, precios, id_anuncios) VALUES ('gastronomia.png','5€', 6);
-INSERT INTO servicios (url_foto_servicio, precios, id_anuncios) VALUES ('gastronomia.png','5€', 7);
+INSERT INTO servicios (url_foto_servicio, precios, id_anuncios_servicios) VALUES ('gastronomia.png','5€', 1);
+INSERT INTO servicios (url_foto_servicio, precios, id_anuncios_servicios) VALUES ('gastronomia.png','5€', 2);
+INSERT INTO servicios (url_foto_servicio, precios, id_anuncios_servicios) VALUES ('gastronomia.png','5€', 3);
+INSERT INTO servicios (url_foto_servicio, precios, id_anuncios_servicios) VALUES ('gastronomia.png','5€', 4);
+INSERT INTO servicios (url_foto_servicio, precios, id_anuncios_servicios) VALUES ('gastronomia.png','5€', 5);
+INSERT INTO servicios (url_foto_servicio, precios, id_anuncios_servicios) VALUES ('gastronomia.png','5€', 6);
+INSERT INTO servicios (url_foto_servicio, precios, id_anuncios_servicios) VALUES ('gastronomia.png','5€', 7);
 
-INSERT INTO dispositivo (nombre, fecha) VALUES ('NombreAlgo1', 06/23/2017);
-INSERT INTO dispositivo (nombre, fecha) VALUES ('NombreAlgo2', 06/23/2017);
-INSERT INTO dispositivo (nombre, fecha) VALUES ('NombreAlgo3', 06/23/2017);
-INSERT INTO dispositivo (nombre, fecha) VALUES ('NombreAlgo4', 06/23/2017);
-INSERT INTO dispositivo (nombre, fecha) VALUES ('NombreAlgo5', 06/23/2017);
-INSERT INTO dispositivo (nombre, fecha) VALUES ('NombreAlgo6', 06/23/2017);
+INSERT INTO dispositivo (nombre_dispositivo, fecha_dispositivo) VALUES ('NombreAlgo1', '06/23/2017');
+INSERT INTO dispositivo (nombre_dispositivo, fecha_dispositivo) VALUES ('NombreAlgo2', '06/23/2017');
+INSERT INTO dispositivo (nombre_dispositivo, fecha_dispositivo) VALUES ('NombreAlgo3', '06/23/2017');
+INSERT INTO dispositivo (nombre_dispositivo, fecha_dispositivo) VALUES ('NombreAlgo4', '06/23/2017');
+INSERT INTO dispositivo (nombre_dispositivo, fecha_dispositivo) VALUES ('NombreAlgo5', '06/23/2017');
+INSERT INTO dispositivo (nombre_dispositivo, fecha_dispositivo) VALUES ('NombreAlgo6', '06/23/2017');
 
 INSERT INTO info () VALUES ();
 INSERT INTO info () VALUES ();
