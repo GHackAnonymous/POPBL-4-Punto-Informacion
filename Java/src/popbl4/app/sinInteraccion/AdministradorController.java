aa3b986e5055257d4a5dd6dd2a560982d05c0ee6
Eder Gomez de Segura <ederaraia@gmail.com>
2017-06-14 15:41:48 +0200

    Anumaciones a log y Añadir
    Aplico las animaciones a los paneles de log y añadir anunciones en el 
    apartado de Administrador.

--- Java/src/popbl4/app/sinInteraccion/AdministradorController.java
+++ Java/src/popbl4/app/sinInteraccion/AdministradorController.java
@@ -9,12 +9,16 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 import popbl4.app.admin.Log;
 import popbl4.app.controladorMain.Controlador;
 
@@ -73,14 +77,17 @@ public class AdministradorController implements Initializable {
              listaLog.getItems().add(lista.get(i));
         }
        }
-        
-        panelAdministrador.setVisible(false);
+        animar(panelAdministrador, "leftCenter");
+        animar(panelLog, "left");
+        //panelAdministrador.setVisible(false);
         panelLog.setVisible(true);
     }
 
     @FXML
     void anadir(ActionEvent event) {
-        panelAdministrador.setVisible(false);
+        animar(panelAdministrador, "leftCenter");
+        animar(panelAnadir, "left");
+        //panelAdministrador.setVisible(false);
         panelAnadir.setVisible(true);
     }
     
@@ -93,17 +100,76 @@ public class AdministradorController implements Initializable {
         
     }
     @FXML
-    void botonAtras(ActionEvent event) {
-        panelAnadir.setVisible(false);
-        panelAdministrador.setVisible(true);
+    void botonAtrasLog(ActionEvent event) {
+
+        animar(panelAdministrador, "right");
+        animar(panelLog, "centeRight");
+        //panelAnadir.setVisible(false);
+        //panelAdministrador.setVisible(true);
     }
     @FXML
-    void botonAtrasLog(ActionEvent event) {
-        panelLog.setVisible(false);
-        panelAdministrador.setVisible(true);
+    void botonAtrasAñadir(ActionEvent event) {
+        
+        animar(panelAdministrador, "right");
+        animar(panelAnadir, "centeRight");       
+        //panelAnadir.setVisible(false);
+        //panelAdministrador.setVisible(true);
     }
     @FXML
     void botonLogout(ActionEvent event) {
 
+    }
+    private void animar(Pane p, String mode) {
+          if(mode.equals("top")){
+           p.setLayoutX(0.0d);
+           p.setLayoutY(-400.0d);
+           Timeline time = new Timeline();
+           time.getKeyFrames().addAll(
+           new KeyFrame(Duration.ZERO, new KeyValue(p.layoutYProperty(), -400.0d)), new KeyFrame(Duration.millis(300.d), new KeyValue(p.layoutYProperty(), 0.0d)));
+           time.play();
+          }
+          if(mode.equals("left")){
+                p.setLayoutX(600.0d);
+                p.setLayoutY(0.0d);
+                Timeline time = new Timeline();
+                time.getKeyFrames().addAll(
+                new KeyFrame(Duration.ZERO, new KeyValue(p.layoutXProperty(), 600.0d)), new KeyFrame(Duration.millis(300.d), new KeyValue(p.layoutXProperty(), 0.0d)));
+                time.play();
+          }
+            if(mode.equals("leftCenter")){
+                p.setLayoutX(0.0d);
+                p.setLayoutY(0.0d);
+               Timeline time = new Timeline();
+           time.getKeyFrames().addAll(
+           new KeyFrame(Duration.ZERO, new KeyValue(p.layoutXProperty(), 0.0d)), new KeyFrame(Duration.millis(300.d), new KeyValue(p.layoutXProperty(), -600.0d)));
+           time.play();
+          }
+          if(mode.equals("up")){
+           p.setLayoutX(0.0d);
+           p.setLayoutY(0.0d);
+           Timeline time = new Timeline();
+           time.getKeyFrames().addAll(
+           new KeyFrame(Duration.ZERO, new KeyValue(p.layoutYProperty(), 0.0d)), new KeyFrame(Duration.millis(300.d), new KeyValue(p.layoutYProperty(), -400.0d)));
+           time.play();
+          }
+          
+          if(mode.equals("centeRight")){
+           p.setLayoutX(0.0d);
+           p.setLayoutY(0.0d);
+           Timeline time = new Timeline();
+           time.getKeyFrames().addAll(
+           new KeyFrame(Duration.ZERO, new KeyValue(p.layoutXProperty(), 0.0d)), new KeyFrame(Duration.millis(300.d), new KeyValue(p.layoutXProperty(), 600.0d)));
+           time.play();
+          }
+          
+           if(mode.equals("right")){
+           p.setLayoutX(-600.0d);
+           p.setLayoutY(0.0d);
+           Timeline time = new Timeline();
+           time.getKeyFrames().addAll(
+           new KeyFrame(Duration.ZERO, new KeyValue(p.layoutXProperty(), -600.0d)), new KeyFrame(Duration.millis(300.d), new KeyValue(p.layoutXProperty(), 0.0d)));
+           time.play();
+          }
+          
     }
 }
