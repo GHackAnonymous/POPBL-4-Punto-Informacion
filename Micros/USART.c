1a6fa774ab4747347530dee076873bf9d081b722
Eder Gomez de Segura <ederaraia@gmail.com>
2017-06-16 17:25:50 +0200

    Comentarios en el archivo usart.c para el sensor de untrasonido

--- Micros/USART.c
+++ Micros/USART.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stm32f407xx.h>
 
+
+//PASAR CLOCK A GPIOA Y USART3
 void initUSART(void){
 	uint32_t *P_RCC_AHB1, *P_RCC_APB1;
 	  
@@ -12,6 +14,10 @@ void initUSART(void){
 	*P_RCC_AHB1 |= (0x01 << POS_GPIOD);
 }
 
+//CONFIGURAR USART
+/*Para cálculos de Mantissa y Div***************************************
+	http://embedded-lab.com/blog/stm32-serial-communication/
+***********************************************************************/
 void USARTconfig (void){	
 	uint32_t * P_USART3_CR1, *P_USART3_BRR;
 	
@@ -26,6 +32,7 @@ void USARTconfig (void){
 	*P_USART3_BRR |= (0x101000101100 << 0);   //Divider y Mantissa 25MHz
 }
 
+//PONER LOS PINES DEL USART EN ALTER MODE
 void USARTGPIOconf(void){
 	uint32_t * P_GPIOD_MODER, *P_GPIOD_AFR;
 	
@@ -42,13 +49,9 @@ void USARTGPIOconf(void){
 	*P_GPIOD_AFR |= (7 << (4 * (PIN_USART3_TX & 7)));
 }
 
-void quitarUSARTRX(void){
-	uint32_t * P_USART3_CR1;
-	
-	P_USART3_CR1 = (uint32_t*) (ADD_USART3 + OFF_CR1);
-	*P_USART3_CR1 &=~ (0x01 << 2);
-}
 
+
+//PARA ENVIAR DATOS POR USART, STATE REGISTER, DATA REGISTER
 void USARTenviar(uint8_t *data, uint32_t size){
 	uint32_t * P_USART3_SR, *P_USART3_DR;
 	
@@ -62,3 +65,80 @@ void USARTenviar(uint8_t *data, uint32_t size){
 	}
 }
 
+
+//DE AQUÍ HACIA ABAJO SON COMENTARIOS DE LO QUE NO SE HA IMPLEMENTADO
+
+// 1- PARA LEER DESDE USART
+
+/*Documentacion************************************************************
+
+http://www.keil.com/forum/61761/cmsis-v5-usartn-irqhandler-multiply-defined
+https://github.com/MaJerle/stm32f429
+
+**************************************************************************/
+
+/*Habilitar una interrupción en NVIC para este handler, por ahora solo con librería stm32f4xx.h
+
+NVIC_DisableIRQ(USART3_IRQn);
+
+*/
+
+/*Llamar a este handler desde la interrupción que se le asigne a la lectura de USART3
+  BUFFERSIZE se tiene que declarar*/
+
+/*void USART3_IRQHandler() {
+	if(bufflen3 < BUFFERSIZE) {
+	usart3_buffer[bufflen3++] = USART3->DR;
+	irqUSART3();
+	} else {
+	bufflen3 = 0;
+	}
+	USART3->SR &= ~USART_SR_RXNE;
+}*/
+
+
+//Esto desactivaría la lectura
+/*void quitarUSARTRX(void){
+	uint32_t * P_USART3_CR1;
+	
+	P_USART3_CR1 = (uint32_t*) (ADD_USART3 + OFF_CR1);
+	*P_USART3_CR1 &=~ (0x01 << 2);
+}*/
+
+
+// 2- PARA UTILIZAR EL SENSOR HC SR04
+
+/*Documentacion*********************************************************************
+
+https://stm32f4-discovery.net/2014/08/library-30-measure-distance-hc-sr04-stm32f4xx/
+https://github.com/MaJerle/stm32f429
+https://hsel.co.uk/2014/08/19/interfacing-the-hc-sr04-ultrasonic-distance-sensor-and-the-stm32f0-discovery-board/
+
+***********************************************************************************/
+
+/*Algoritmia************************************************************************
+
+1- Conectar HC-SR04 a 5v de alimentación, GND, PAx y PAz
+2- Activar GPIOA de donde estén conectados el TRIGGER y el ECHO
+3- En algún momento del programa lanzar un 1 al TRIGGER
+4- Escuchar con la misma función que para el sensor de infrarrojos al ECHO
+5- Empezar un contador cuando ECHO suba a 1
+6- Terminar de contar cuando ECHO baje a 0 (Devolver valor en TicksRegistrados)
+7- Leer cuantos ticks a hecho el clock
+8- Si es inferior a una cantidad de ticks, según la distancia, USARTenviar
+9- Si es superior a una cantidad se ticks, según la distancia, no hacer nada
+
+
+************************************************************************************/
+
+/*Para calcular la distancia al objeto***********************************************
+
+Tomamos el preescaler del clock como 1:
+
+TiempoPorTick = 1/FrecuenciaReloj
+TiempoDelPulso = TiempoPorTick * TicksRegistrados
+TiempoDeVueloDeSeñal = TiempoDelPulso/2
+DistanciaAlObjeto = TiempoDeVueloSeñal * 343  (El sonido viaja a 343m/s)
+
+
+************************************************************************************/
